3b38a32bbd6cefcd934e89b85572936eb5983e2d
Berzel <berzelbtumbude@gmail.com>
2020-02-07 11:24:32 +0200

    Updates

--- src/redux/middleware/saver.js
+++
@@ -1,7 +0,0 @@
-const saver = store => next => action => {
-  const result = next(action)
-  localStorage.setItem('redux-store', JSON.stringify(store.getState()))
-  return result
-}
-
-export default saver
